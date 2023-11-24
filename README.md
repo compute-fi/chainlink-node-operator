43e334701c045f6d2e184cc64afba0711024853b
madhukar123456 <shroudmg@gmail.com>
2023-11-24 07:17:31 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,74 @@
+
+
+# Commands
+
+Elevate to Root 
+sudo su -
+
+Install Docker on Ubuntu
+
+curl -sSL https://get.docker.com/ | sh
+sudo usermod -aG docker $USER
+exit
+
+Make a ChainLink Folder
+mkdir ~/.chainlink
+
+Create an Account (Ethereum Node) on Alchemy
+https://www.alchemy.com/
+https://dashboard.alchemyapi.io/
+
+Install Light Weight Postgres Client
+sudo apt install -y postgresql-client
+
+Test Database Connection to Google Cloud SQL
+pg_isready -d chainlinkdb2 -h 35.223.13.41 -p 5432 -U postgres
+
+Create .env config file
+ROOT=/chainlink
+LOG_LEVEL=debug
+ETH_CHAIN_ID=1
+CHAINLINK_TLS_PORT=0
+SECURE_COOKIES=false
+ALLOW_ORIGINS=*
+ETH_URL=
+DATABASE_URL=
+
+
+Download and Run the Chain Link Node via Docker
+docker run -p 6688:6688 -v ~/.chainlink:/chainlink -it --env-file=.env smartcontract/chainlink:1.7.0-root local n
+
+Install NGINX
+apt-get install nginx
+
+Modify NGINX Server Block
+
+ location ~ / {
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header Host $http_host;
+    proxy_redirect off;
+    proxy_pass http://localhost:6688;
+ }
+
+service nginx reload
+service nginx restart
+
+Keystore Credential Requirements
+- Don’t use numbers
+- 4 uppercase letters
+- Greater than 16 chars
+
+# Chainlink Node Operator creds
+
+Hosted on  :
+`
+http://34.125.213.96/
+`
+
+# Request compute steps
+
+1. Deploy the RequestComputeGPU.sol and RequestOutputLog.sol and fill them with Goerli LINK tokens 5-10.
+
+oracle_address: 0xd0905cB54D3934F4c746AE1c15de4662310993C0
+jobID: 4dfdc7e76f114404a71489de493d88f4
+example fileURL: `https://raw.githubusercontent.com/madhukar123456/kaggle-kernel/main/python-code/mistraltest.ipynb`
---
+++ RequestComputeGPU.sol
@@ -0,0 +1,105 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+
+/**
+ * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
+ * DO NOT USE THIS CODE IN PRODUCTION.
+ */
+
+contract RequestComputeGPU is ChainlinkClient, ConfirmedOwner {
+    using Chainlink for Chainlink.Request;
+
+    uint256 private constant ORACLE_PAYMENT = (1 * LINK_DIVISIBILITY) / 10; // 0.1 * 10**18
+    // uint256 public currentPrice;
+    string public currentReceivedID;
+
+    event RequestComputeFulfilled(
+        bytes32 indexed requestId,
+        // uint256 indexed price
+        string indexed receivedID
+    );
+
+    /**
+     *  Goerli
+     *@dev LINK address in Goerli network: 0x326C977E6efc84E512bB9C30f76E30c160eD06FB
+     * @dev Check https://docs.chain.link/docs/link-token-contracts/ for LINK address for the right network
+     */
+    constructor() ConfirmedOwner(msg.sender) {
+        setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
+    }
+
+    function requestCompute(
+        address _oracle,
+        string memory _jobId,
+        string memory _fileUrl
+    ) public onlyOwner {
+        Chainlink.Request memory req = buildChainlinkRequest(
+            stringToBytes32(_jobId),
+            address(this),
+            this.fulfillComputeRequest.selector
+        );
+        // Create a bytes array to store the concatenated string
+        bytes memory url = abi.encodePacked(
+            "https://kkpy.onrender.com/compute?fileUrl=",
+            _fileUrl
+        );
+
+        req.add(
+            "get",
+            string(url)
+        );
+        req.add("path", "generatedfolder");
+        sendChainlinkRequestTo(_oracle, req, ORACLE_PAYMENT);
+    }
+
+    function fulfillComputeRequest(
+        bytes32 _requestId,
+        string memory _receivedID
+    ) public recordChainlinkFulfillment(_requestId) {
+        emit RequestComputeFulfilled(_requestId, _receivedID);
+        currentReceivedID = _receivedID;
+    }
+
+    function getChainlinkToken() public view returns (address) {
+        return chainlinkTokenAddress();
+    }
+
+    function withdrawLink() public onlyOwner {
+        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+        require(
+            link.transfer(msg.sender, link.balanceOf(address(this))),
+            "Unable to transfer"
+        );
+    }
+
+    function cancelRequest(
+        bytes32 _requestId,
+        uint256 _payment,
+        bytes4 _callbackFunctionId,
+        uint256 _expiration
+    ) public onlyOwner {
+        cancelChainlinkRequest(
+            _requestId,
+            _payment,
+            _callbackFunctionId,
+            _expiration
+        );
+    }
+
+    function stringToBytes32(
+        string memory source
+    ) private pure returns (bytes32 result) {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+            return 0x0;
+        }
+
+        assembly {
+            // solhint-disable-line no-inline-assembly
+            result := mload(add(source, 32))
+        }
+    }
+}
